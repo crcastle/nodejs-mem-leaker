d894f0bc7e1bdb68613a7f349adbdc6cda808bee
Chris Castle <chris@render.com>
2021-12-22 17:27:45 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const leak = [];
+
+const server = http.createServer((req, res) => {
+    leak.push(new Array(1e6).fill("some string"))
+
+    const used = process.memoryUsage();
+    for (let key in used) {
+      console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+    }
+    console.log('---')
+
+    res.end('' + leak.length)
+});
+
+server.listen(3000)
+console.log("Server listening to port 3000. Press Ctrl+C to stop it.")
