ee4911f6d4905c4d99d131fe74f593ee54a6b744
Chris Castle <chris@render.com>
2021-12-22 17:49:36 -0500

    Log request count

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http')
 
 const leak = [];
+let requestCount = 0;
 
 const server = http.createServer((req, res) => {
     leak.push(new Array(1e6).fill("some string"))
 
     const used = process.memoryUsage();
+    console.log(`Request ${requestCount} (since process started)`)
+    requestCount++
     for (let key in used) {
       console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
     }
